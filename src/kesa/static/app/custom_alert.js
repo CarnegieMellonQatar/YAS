2b1ed9573af431b803ba3306c6295959a3df5ac0
yelsayed <yelsayed@qatar.cmu.edu>
2016-04-15 15:25:32 +0300

    CSS revamp for most of the webpage using SCSS

--- src/kesa/static/app/custom_alert.js
+++
@@ -1,7 +0,0 @@
-
-function customAlert(string) {
-    $("body").append('<div class="custom-alert animated fadeInRightBig">' + string + '</div>');
-    setTimeout(function() {
-        $(".custom-alert").remove();
-    }, 3000);
-}
