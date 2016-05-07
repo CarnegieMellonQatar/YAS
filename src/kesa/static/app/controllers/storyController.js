66d82435d111929a20b68262d3437fcf7964e1a4
Sharjeel-Khan <smkhan@qatar.cmu.edu>
2016-05-07 14:25:06 +0300

    Made only one request for Info

--- src/kesa/static/app/controllers/storyController.js
+++ src/kesa/static/app/controllers/storyController.js
@@ -1,8 +1,8 @@
-(function () {
+(function() {
     "use strict";
 
     angular.module('storyTeller')
-        .controller('storyController', function (storyService) {
+        .controller('storyController', function(storyService) {
 
             var story = this;
             var isLiked = null;
@@ -14,75 +14,69 @@
             story.enabled = true;
 
             /************* CallBack Functions ***************/
-            story.gI = function (err, data, stories) {
+            story.gI = function(err, data, allstories) {
                 if (err) {
                     console.log("error in getting data");
-                }
-                else {
-                    stories.fields.userInfo = JSON.parse(data['u'])[0];
+                } else {
+                    var i = 0;
+                    var datalist = data;
+                    for (i = 0; i < datalist.length; i++) {
+                        var data = datalist[i];
+                        var stories = allstories[i];
 
-                    if (data['rl'] === 'true') {
-                        stories.fields.isBookmarked = true;
-                    } else {
-                        stories.fields.isBookmarked = false;
+                        stories.fields.userInfo = JSON.parse(data['u'])[0];
+
+                        if (data['rl'] === 'true') {
+                            stories.fields.isBookmarked = true;
+                        } else {
+                            stories.fields.isBookmarked = false;
+                        }
+
+                        stories.fields.likes = JSON.parse(data['l']);
+
+                        if (data['liked'] === 'true'){
+                            stories.fields.isLiked = true;
+                        } else {
+                            stories.fields.isLiked = false;
+                        }
+
+                        stories.fields.contributors = JSON.parse(data['c']);
+
+                        if (stories.fields.is_complete) {
+                            story.completeStories.push(stories);
+                        } else {
+                            story.uncompleteStories.push(stories);
+                        }
                     }
 
-                    var liked = JSON.parse(data['l']);
-                    stories.fields.likes = liked.length;
-                    var isLiked = liked.reduce(
-                        function (pV, cV, cI, array) {
-                            if (pV) {
-                                return pV;
-                            } else {
-                                return (cV.fields.user === story.profile.pk);
-                            }
-                        }, false);
-                    stories.fields.isLiked = isLiked;
+                    if (story.completeStories.length < 10) {
+                        story.haveMoreComplete = false;
+                    }
 
-                    stories.fields.contributors = JSON.parse(data['c']).length;
+                    if (story.uncompleteStories.length < 10) {
+                        story.haveMoreIncomplete = false;
+                    }
                 }
             };
             /**************************************************/
 
-            storyService.getUserByRequest(function (err, data) {
-                    if (err) {
-                        console.log("error in getting data");
-                    } else {
-                        story.profile = data[0];
-                        storyService.getfirst(function (err, data) {
-                            if (err) {
-                                console.log("error in getting data");
-                            }
-                            else {
-                                var i = 0;
-                                for (i = 0; i < data.length; i++) {
-                                    if (data[i].fields.is_complete) {
-                                        story.completeStories.push(data[i]);
-
-                                        storyService.getInfo(data[i],story.gI);
-                                    }
-                                    else {
-                                        story.uncompleteStories.push(data[i]);
-
-                                        storyService.getInfo(data[i],story.gI);
-                                    }
-                                }
-
-                                if (story.completeStories.length < 10) {
-                                    story.haveMoreComplete = false;
-                                }
-
-                                if (story.uncompleteStories.length < 10) {
-                                    story.haveMoreIncomplete = false;
-                                }
-                            }
-                        });
-                    }
+            storyService.getUserByRequest(function(err, data) {
+                if (err) {
+                    console.log("error in getting data");
+                } else {
+                    story.profile = data[0];
+                    storyService.getfirst(function(err, data) {
+                        if (err) {
+                            console.log("error in getting data");
+                        } else {
+                            var i = data.length - 1;
+                            storyService.getInfo(data[0].pk, data[i].pk, 2, data, "None", story.gI);
+                        }
+                    });
                 }
-            )
-            ;
+            });
 
-            story.changeTabs = function (type) {
+            story.changeTabs = function(type) {
                 if (type) {
                     story.enabled = true;
                 } else {
@@ -90,66 +84,55 @@
                 }
             };
 
-            story.getMoreCompleteStories = function () {
+            story.getMoreCompleteStories = function() {
                 var last = story.completeStories[story.completeStories.length - 1];
-                storyService.getCompleted(last.pk, 10, function (err, data) {
+                storyService.getCompleted(last.pk, 10, function(err, data) {
                     if (err) {
                         console.log("error in getting data");
-                    }
-                    else {
-                        var i = 0;
+                    } else {
+                        var i = data.length - 1;
 
                         if (data.length < 10) {
                             story.haveMoreComplete = false;
                         }
 
-                        for (i = 0; i < data.length; i++) {
-                            story.completeStories.push(data[i]);
-
-                            storyService.getInfo(data[i],story.gI);
-                        }
+                        storyService.getInfo(data[0].pk, data[i].pk, 1, data, "None", story.gI);
                     }
                 });
             };
 
-            story.getMoreInCompleteStories = function () {
+            story.getMoreInCompleteStories = function() {
                 var last = story.uncompleteStories[story.uncompleteStories.length - 1];
-                storyService.getIncompleted(last.pk, 10, function (err, data) {
+                storyService.getIncompleted(last.pk, 10, function(err, data) {
                     if (err) {
                         console.log("error in getting data");
-                    }
-                    else {
-                        var i = 0;
+                    } else {
+                      var i = data.length - 1;
 
-                        if (data.length < 10) {
-                            story.haveMoreIncomplete = false;
-                        }
+                      if (data.length < 10) {
+                          story.haveMoreComplete = false;
+                      }
 
-                        for (i = 0; i < data.length; i++) {
-                            story.uncompleteStories.push(data[i]);
-
-                            storyService.getInfo(data[i],story.gI);
-                        }
+                      storyService.getInfo(data[0].pk, data[i].pk, 0, data, "None", story.gI);
                     }
                 });
             };
 
-            story.getStatus = function (data) {
+            story.getStatus = function(data) {
                 if (data.is_complete) {
                     return "Complete";
                 } else {
                     if (data.is_open) {
                         return "Active";
-                    }
-                    else {
+                    } else {
                         return "Closed";
                     }
 
                 }
             };
 
-            story.like = function (data) {
-                storyService.Like(story.profile.pk, data, function (err, data, cstory) {
+            story.like = function(data) {
+                storyService.Like(story.profile.pk, data, function(err, data, cstory) {
                     if (err) {
                         console.log("error in getting data");
                     } else {
@@ -161,8 +144,8 @@
                 });
             };
 
-            story.unlike = function (data) {
-                storyService.Unlike(story.profile.pk, data, function (err, data, cstory) {
+            story.unlike = function(data) {
+                storyService.Unlike(story.profile.pk, data, function(err, data, cstory) {
                     if (err) {
                         console.log("error in getting data");
                     } else {
@@ -174,8 +157,8 @@
                 });
             };
 
-            story.bookmark = function (data) {
-                storyService.addToReadLater(story.profile.pk, data, function (err, data, cstory) {
+            story.bookmark = function(data) {
+                storyService.addToReadLater(story.profile.pk, data, function(err, data, cstory) {
                     if (err) {
                         console.log("error in getting data");
                     } else {
@@ -186,8 +169,8 @@
                 });
             };
 
-            story.unbookmark = function (data) {
-                storyService.removeFromReadLater(story.profile.pk, data, function (err, data, cstory) {
+            story.unbookmark = function(data) {
+                storyService.removeFromReadLater(story.profile.pk, data, function(err, data, cstory) {
                     if (err) {
                         console.log("error in getting data");
                     } else {
